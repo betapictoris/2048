5ca745d24834f3023facdf2f7193a4a4d85768c5
Daniel Hall <beta@hai.haus>
2024-11-23 00:10:23 -0800

    feat: Created setting of grid value

--- assets/game.js
+++ assets/game.js
@@ -1,6 +1,13 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
+grid = [
+  [0,0,0,0],
+  [0,0,0,0],
+  [0,0,0,0],
+  [0,0,0,0]
+]
+
 function drawSquare(x, y) {
   ctx.fillRect(
     x * 16 + 185 * (x - 1),
@@ -8,4 +15,17 @@ function drawSquare(x, y) {
     185,
     185
   );  
+}
+
+function setVaultAtSquare(x, y, stage) {
+  value = 2 ** stage;
+  grid[y-1][x-1] = value;
+  console.log(grid)
+
+  ctx.fillStyle = "#C9F3C9";
+  drawSquare(x, y)
+
+  ctx.fillStyle = "#000000";
+  ctx.font = "48px sans-serif";
+  ctx.fillText(value, x * 16 + 185 * (x - 1) + 80,  y * 16 + 185 * (y - 1) + 104);
 }
