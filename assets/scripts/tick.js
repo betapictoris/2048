c3066fc9ae13166193cf3b54cc392c1e4d02dd0e
Daniel Hall <beta@hai.haus>
2024-11-27 12:09:18 -0800

    feat: Use localStorage for the highscore instead of a cookie

--- assets/scripts/tick.js
+++ assets/scripts/tick.js
@@ -45,33 +45,10 @@ function calculateScore() {
     }
   }
 
-  if (score > Number(getCookie("highscore"))) {
-    setCookie("highscore", score, 30);
+  if (score > Number(localStorage.getItem("highscore"))) {
+    localStorage.setItem("highscore", score);
   }
 
   document.getElementById("score").innerText = score;
-  document.getElementById("highscore").innerText = getCookie("highscore");
-}
-
-function getCookie(cname) {
-  let name = cname + "=";
-  let decodedCookie = decodeURIComponent(document.cookie);
-  let ca = decodedCookie.split(";");
-  for (let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == " ") {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
-}
-
-function setCookie(cname, cvalue, exdays) {
-  const d = new Date();
-  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
-  let expires = "expires=" + d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  document.getElementById("highscore").innerText = localStorage.getItem("highscore");
 }
