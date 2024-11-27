71007afd79a72ac2e46d199d6d24d0b0a0a14b4d
Daniel Hall <beta@hai.haus>
2024-11-27 12:27:28 -0800

    feat: Use classic 2048 game over check

--- assets/scripts/tick.js
+++ assets/scripts/tick.js
@@ -27,11 +27,57 @@ function moveRight() {
 }
 
 function checkGameOver() {
-  if (grid.every((val) => val.every((val) => val >= 1))) {
-    alert("Game over");
-    window.location.reload();
-    throw "Game over, ending event."; // Stops memory leak. ¯\_(ツ)_/¯
+  isOver = true;
+
+  for (let gridX = 0; gridX < 4 && isOver; gridX++) {
+    for (let gridY = 0; gridY < 4 && isOver; gridY++) {
+      value = grid[gridY][gridX]
+
+      if (value === 0) {
+        isOver = false;
+      }
+
+      neighbors = []
+
+      if (typeof grid[gridY+1] != "undefined") {
+        neighbors.push(grid[gridY+1][gridX])
+      }
+
+      if (typeof grid[gridY][gridX+1] != "undefined") {
+        neighbors.push(grid[gridY][gridX+1])
+      }
+
+      if (typeof grid[gridY-1] != "undefined") {
+        neighbors.push(grid[gridY-1][gridX])
+      }
+
+      if (typeof grid[gridY][gridX-1] != "undefined") {
+        neighbors.push(grid[gridY][gridX-1])
+      }
+
+      if (neighbors.includes(value)) {
+        isOver = false;
+      }
+    }
   }
+
+  if (isOver) {
+    endGame();
+  }
+}
+
+function endGame() {
+  highscore = Number(localStorage.getItem("highscore"))
+  // Make sure to update the highscore as needed.
+  if (score >= highscore) {
+    localStorage.setItem("highscore", score);
+    alert(`Game over!\nNew highscore: ${highscore}!`)
+  } else {
+    alert(`Game over!`)
+  }
+
+  window.location.reload();
+  throw "Game over, ending event."; // Stops memory leak. ¯\_(ツ)_/¯
 }
 
 function checkHighScore() {
